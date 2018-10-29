a930401017c0ec0fc683000d0ae22783ecc15f5e
Yarisson18 <gemini8811@gmail.com>
2018-10-29 04:12:39 +0300

    добавление плагинов для сборки проекта и модификация сборки

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,14 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var htmlmin = require("gulp-htmlmin");
+var imagemin = require("gulp-imagemin");
+var posthtml = require("gulp-posthtml");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var webp = require("gulp-webp");
+var include = require("posthtml-include");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -15,7 +23,31 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(server.stream());
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("server", function () {
@@ -27,8 +59,47 @@ gulp.task("server", function () {
     ui: false
   });
 
+  gulp.task("webp", function() {
+    return gulp.src("source/img/**/*.{png,jpg}")
+      .pipe(webp({quality: 90}))
+      .pipe(gulp.dest("source/img"));
+  });
+
+  gulp.task("sprite", function() {
+    return gulp.src("source/img/**/sp-*.svg")
+      .pipe(svgstore({
+        inlineSvg: true
+      }))
+      .pipe(rename("sprite.svg"))
+      .pipe(gulp.dest("build/img"));
+  });
+
+
+  gulp.task("html", function() {
+    return gulp.src("source/*.html")
+      .pipe(posthtml([
+        include()
+      ]))
+      .pipe(htmlmin({
+        collapseWhitespace: true
+      }))
+      .pipe(gulp.dest("build"));
+  });
+
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("build", function(done) {
+  run(
+    "images",
+    "webp",
+    "copy",
+    "css",
+    "sprite",
+    "html",
+    done
+  );
+});
