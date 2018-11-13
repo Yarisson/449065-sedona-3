25c241a1597be83ab839fcaf60e5382a34cf6897
Yarisson18 <gemini8811@gmail.com>
2018-11-13 06:39:59 +0300

    правки по замечаниям

---
+++ source/js/map.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function () {
+  var mapHidden = document.querySelector('.hotel-interest__map--background-hidden');
+  var iframeHidden = document.querySelector('.hotel-interest__map-hidden');
+
+  mapHidden.classList.remove('hotel-interest__map');
+  iframeHidden.classList.remove('hotel-interest__map-hidden');
+})();
